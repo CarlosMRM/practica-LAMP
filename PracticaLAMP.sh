0aec623adf6ee3e7fa4180bfc1c36ce402ff72d5
carlomrm <carlossscs01@gmail.com>
2020-10-24 19:50:59 +0200

    soluciono error con ruta script

--- PracticaLAMP.sh
+++ PracticaLAMP.sh
@@ -42,7 +42,9 @@ phpinfo();
 #composer update
 #cd ..
 #sudo mv phpmyadmin/ /var/www/html/
-sudo ./scripts/instalaComposer.sh
+cd ./scripts
+sudo ./instalaComposer.sh
+cd ..
 #|------------------------>Instalación de GoAccess <-----------------------------|
 echo "deb http://deb.goaccess.io/ $(lsb_release -cs) main" | sudo tee -a /etc/apt/sources.list.d/goaccess.list
 wget -O - https://deb.goaccess.io/gnugpg.key | sudo apt-key add -
